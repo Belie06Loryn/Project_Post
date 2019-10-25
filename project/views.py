654fd6679abfdd6b7c743c5fd1013a09ad9d254c
Belie06Loryn <maniralexie@gmail.com>
2019-10-25 11:54:05 +0200

    Register,Login with navbar

---
+++ project/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+
+def page(request):
+    return render(request,'all-projects/index.html',{})
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alexie/Desktop/Project_Post/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
