b7051e848877d3f79b2e9aec8f46a7e6f95e9876
Belie06Loryn <maniralexie@gmail.com>
2019-10-30 20:02:30 +0200

    testing profile class

--- project/tests.py
+++ project/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Profile,Foto
 
-# Create your tests here.
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.wandi = Profile( image='default.jpg',email='mmab@gma.co',bio='heey its me', link='www.john.me',country='rwanda')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.wandi,Profile))
+
+    def test_save(self):
+        self.wandi.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles)>0)
+ 
+    def test_upd(self):
+        wandi = Profile.objects.filter(id=1)
+        wandi.update(image = 'Kam.jpeg', email ='james@morischool.com',bio = 'hdefedf')
+        search = Profile.objects.filter(id=1)
+        self.assertNotEqual(search,'Kam.jpeg')
+
+    def test_dele(self):
+        self.wandi.save_profile()
+        profi = Profile.objects.all()
+        self.assertTrue(len(profi)>=0)  
+
