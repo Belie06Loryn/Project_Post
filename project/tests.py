e1f310ae729629fb8384fc5c6f7662f41bfeafc5
Belie06Loryn <maniralexie@gmail.com>
2019-10-30 20:03:09 +0200

    testing project class

--- project/tests.py
+++ project/tests.py
@@ -25,3 +25,37 @@ class ProfileTestClass(TestCase):
         profi = Profile.objects.all()
         self.assertTrue(len(profi)>=0)  
 
+class FotoTestClass(TestCase):
+
+    def setUp(self):
+        self.profile= Profile(image='default.jpg',email='mmab@gma.co',bio='heey its me', link='www.john.me',country='rwanda')
+        self.profile.save_profile()
+
+        self.new_photos= Foto(image = 'Jam.jpeg', sitename ='Muriuki', url ='jamesmoringaschoolcom',description = "sdfghyjukiljhgf")
+        self.new_photos.save_pic()      
+
+    def test_save_pick(self):
+        self.new_photos= Foto(image = 'Jam.jpeg', sitename ='Muriuki', url ='jamesmoringaschoolcom',description = "sdfghyjukiljhgf")
+        self.new_photos.save_pic()
+        picture = Foto.objects.all()
+        self.assertTrue(len(picture)>=1)
+
+    def test_dele_pick(self):
+        self.new_photos= Foto(image = 'Jam.jpeg', sitename ='Muriuki', url ='jamesmoringaschoolcom',description = "sdfghyjukiljhgf")
+        self.new_photos.save_pic()
+        picture = self.new_photos.dele_pic()
+        delete = Foto.objects.all()
+        self.assertTrue(len(delete)>=0)   
+
+    def test_upd_pic(self):
+        image = Foto.objects.filter(id=1)
+        image.update(sitename ='lez.jpeg')
+        search = Foto.objects.filter(id=1)
+        self.assertNotEqual(search,'lez.jpeg')     
+        
+    def test_pic_id(self):
+        self.image = Foto(image = 'Jam.jpeg', sitename ='Muriuki', url ='jamesmoringaschoolcom',description = "sdfghyjukiljhgf")
+        self.image.save_pic()
+        search = Foto.image_by_id(self.image.id)
+        self.assertNotEqual(search,self.image)
+
